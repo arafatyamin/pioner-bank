2247b51d2280da5b80c601db611aa5771c43ccff
arafatyamin <mdh631987@gmail.com>
2022-08-23 23:18:58 +0600

    updated login js

--- js/logIn.js
+++ js/logIn.js
@@ -1,12 +1,13 @@
-document.getElementById('submit-btn').addEventListener('click', function () {
+document.getElementById('submit-btn').addEventListener('click',function(){
 
     const emailField = document.getElementById('email-field');
     const email = emailField.value;
     const passwordField = document.getElementById('password-field');
     const password = passwordField.value;
-    if (email.endsWith === '@gmail.com' && password === '12345') {
+    if(email.endsWith('@gmail.com' )){
         window.location.href = 'bank.html'
-    } else {
-        alert('your password is incorrect');
+    }
+    else{
+        alert('your gmail is incorrect');
     }
 });
